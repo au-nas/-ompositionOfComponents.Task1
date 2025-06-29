041770fb2b6b7e6b91c96cb4336d8a1e5f63bcb0
Anastasiia Vasilchenko <nastyavasilshenko@mail.ru>
2025-06-29 17:47:45 +0300

    feat: реализована логика хода и победы

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -4,16 +4,19 @@ import { Field } from './Field.jsx';
 import { useState } from 'react';
 import styles from '../styles/Game.module.css';
 
-const WIN_PATTERNS = [
-	[0, 1, 2],
-	[3, 4, 5],
-	[6, 7, 8],
-	[0, 3, 6],
-	[1, 4, 7],
-	[2, 5, 8],
-	[0, 4, 8],
-	[2, 4, 6],
-];
+const checkWin = (field, currentPlayer) => {
+	const WIN_PATTERNS = [
+		[0, 1, 2], [3, 4, 5], [6, 7, 8], // варианты побед по горизонтали
+		[0, 3, 6], [1, 4, 7], [2, 5, 8], // варианты побед по вертикали
+		[0, 4, 8], [2, 4, 6] // варианты побед по диагонали
+	];
+
+	return WIN_PATTERNS.some((patterns) => {
+		return patterns.every((index) => {
+            return field[index] === currentPlayer
+		})
+	})
+}
 
 export const Game = () => {
 	const [currentPlayer, setCurrentPlayer] = useState('X');
@@ -22,7 +25,20 @@ export const Game = () => {
 	const [field, setField] = useState(['', '', '', '', '', '', '', '', '']);
 
 	const handleClickField = (index) => {
-		console.log('click', index);
+		// console.log('click', index);
+
+
+		const copyField = [...field]; // копия, посмотрела в созовне
+		copyField[index] = currentPlayer;
+setField(copyField);
+
+        if (checkWin(copyField, currentPlayer)) {
+			return setIsGameEnded(true);
+		} else if (!copyField.includes('')) {
+			return setIsDraw(true);
+		}
+
+		setCurrentPlayer((prevState) => prevState === 'X' ? '0' : 'X')
 	};
 
 	return (
